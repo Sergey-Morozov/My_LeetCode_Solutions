36897b74d2c2728e857a1e608b4d3c9dfa1269d7
Sergey Morozov <dr.s.morozov@gmail.com>
2026-09-21 18:55:32 +0200

    Time: 0 ms (100%), Space: 11.3 MB (20.04%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,29 @@
+// Author: Sergey Morozov
+// https://leetcode.com/u/Sergey-Morozov/
+// https://github.com/Sergey-Morozov
+// https://www.linkedin.com/in/sergey-morozov-6ba2705/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *tmp;
+        if ((head == nullptr) || (head->next) == nullptr) 
+            return head;
+        else {
+            tmp = head->next;
+            head->next = swapPairs(head->next->next);
+            tmp->next = head;
+            return tmp;
+        };
+    }
+};
