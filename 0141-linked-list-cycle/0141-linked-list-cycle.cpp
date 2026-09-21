468d63c13c6802e7cc52d619f7af6d5558b33518
Sergey Morozov <dr.s.morozov@gmail.com>
2026-09-21 20:56:08 +0200

    Time: 10 ms (46.16%), Space: 11.9 MB (52.93%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,25 @@
+// Author: Sergey Morozov
+// https://leetcode.com/u/Sergey-Morozov/
+// https://github.com/Sergey-Morozov
+// https://www.linkedin.com/in/sergey-morozov-6ba2705/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr) return false;
+        for (int i = 0; i < 10000; i++) {
+            if (head->next == nullptr)
+                return false;
+            head = head->next;
+        }
+        return true;
+    }
+};
