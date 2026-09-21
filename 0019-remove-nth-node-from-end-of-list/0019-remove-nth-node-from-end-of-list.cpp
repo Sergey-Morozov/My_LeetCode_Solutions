23c1395f7db153dc8446f536c6393e9bb7e028f9
Sergey Morozov <dr.s.morozov@gmail.com>
2026-09-21 18:48:50 +0200

    Time: 0 ms (100%), Space: 15 MB (25.77%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,32 @@
+// Author: Sergey Morozov
+// https://leetcode.com/u/Sergey-Morozov/
+// https://github.com/Sergey-Morozov
+// https://www.linkedin.com/in/sergey-morozov-6ba2705/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* curr = head;
+        for (int i = 0; i < n; i++) {
+            curr = curr->next;
+        };
+        ListNode* prev = head;
+        if (curr == nullptr) return head->next;
+        while (curr->next != nullptr) {
+            curr = curr->next;
+            prev = prev->next;
+        }
+        prev->next = prev->next->next;
+        return head;
+    }
+};
