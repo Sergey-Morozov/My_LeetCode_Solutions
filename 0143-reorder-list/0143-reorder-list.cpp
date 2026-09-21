6f6ca447af09a75e2dda979c274cb4893fb73f3d
Sergey Morozov <dr.s.morozov@gmail.com>
2026-09-21 21:13:40 +0200

    Time: 1 ms (13.98%), Space: 27.9 MB (5.73%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,45 @@
+// Author: Sergey Morozov
+// https://leetcode.com/u/Sergey-Morozov/
+// https://github.com/Sergey-Morozov
+// https://www.linkedin.com/in/sergey-morozov-6ba2705/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return;
+        vector<ListNode*> all(50000, nullptr);
+        ListNode *curr = head;
+        int n = 0;
+        do {
+            all[n] = curr;
+            curr = curr->next;
+            n++;
+        } while (curr != nullptr);
+        int i = 0;
+        n--;
+        curr = head;
+        while (i < n) {
+            curr->next = all[n];
+            n--;
+            curr = curr->next;
+            i++;
+            if (i > n)
+                break;
+            curr->next = all[i];
+            curr = curr->next;
+        }
+        curr->next = nullptr;
+        return;
+    }
+};
