28cbe48ceda4603d3dbc05181f693a7d9ac2f87b
Sergey Morozov <dr.s.morozov@gmail.com>
2026-09-21 20:10:26 +0200

    Time: 0 ms (100%), Space: 16.1 MB (91.5%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,31 @@
+// Author: Sergey Morozov
+// https://leetcode.com/u/Sergey-Morozov/
+// https://github.com/Sergey-Morozov
+// https://www.linkedin.com/in/sergey-morozov-6ba2705/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr)
+            return nullptr;
+        ListNode* curr = head;
+        while (curr->next != nullptr) {
+            while (curr->next != nullptr && (curr->next)->val == curr->val)
+                curr->next = (curr->next)->next;
+            if (curr->next == nullptr)
+                break;
+            curr = curr->next;
+        }
+        return head;
+    }
+};
